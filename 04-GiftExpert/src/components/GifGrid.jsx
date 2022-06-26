ac82351013e94a645e9f70a7b2f76d7c116e5391
Arturo Quirós <joseq2408@gmail.com>
2022-06-26 17:45:27 -0600

    useEffect implemented

--- 04-GiftExpert/src/components/GifGrid.jsx
+++ 04-GiftExpert/src/components/GifGrid.jsx
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 const GifGrid = ({ category }) => {
-  getGifs(category);
+  useEffect(() => {
+    getGifs(category);
+  }, []);
 
   return (
     <>
--- 04-GiftExpert/src/main.jsx
+++ 04-GiftExpert/src/main.jsx
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom/client";
 import GiftExpertApp from "./GiftExpertApp";
 import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <GiftExpertApp />
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<GiftExpertApp />);
