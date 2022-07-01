b21f6c7bd0c520a51c547a72b47d75c5faf90fec
Arturo Quirós <joseq2408@gmail.com>
2022-06-30 21:48:17 -0600

    using map for gifs done

--- 04-GiftExpert/src/components/GifGrid.jsx
+++ 04-GiftExpert/src/components/GifGrid.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 const GifGrid = ({ category }) => {
+  const [images, setImages] = useState([]);
+  const getImages = async () => {
+    const newImages = await getGifs(category);
+    setImages(newImages);
+  };
+
   useEffect(() => {
-    getGifs(category);
+    getImages();
   }, []);
 
   return (
     <>
       <h3>{category}</h3>
+      {images.map(({ id, title }) => (
+        <ul>
+          <li key={id}>{title}</li>
+        </ul>
+      ))}
     </>
   );
 };
