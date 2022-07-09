5c05913a4a501d63ff4da42ea1d79e7ea852ebdd
Arturo Quirós <joseq2408@gmail.com>
2022-07-09 17:31:57 -0600

    search page 1/2

--- 06-HeroesSPA/src/heroes/pages/SearchPage.jsx
+++ 06-HeroesSPA/src/heroes/pages/SearchPage.jsx
@@ -3,7 +3,31 @@ import React from "react";
 export const SearchPage = () => {
   return (
     <>
-      <h1>Search heroe</h1>
+      <h1>Search </h1>
+      <hr></hr>
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Searching</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+            <button className="btn btn-outline-primary mt-1">Search</button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr></hr>
+          <div className="alert alert-primary">Search a Hero</div>
+          <div className="alert alert-danger">No Heros found</div>
+        </div>
+      </div>
     </>
   );
 };
