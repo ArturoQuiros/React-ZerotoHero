fabe089a28fff11d378657b0423e425c6f83e149
Arturo Quirós <joseq2408@gmail.com>
2022-07-08 22:19:13 -0600

    routes files

--- 06-HeroesSPA/src/router/AppRouter.jsx
+++ 06-HeroesSPA/src/router/AppRouter.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { LoginPage } from "../auth/pages/LoginPage";
-import { DcPage } from "../heroes/pages/DcPage";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
+import { LoginPage } from "../auth";
+import { DcPage, MarvelPage } from "../heroes";
+import { Navbar } from "../ui";
 
 export const AppRouter = () => {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="marvel" element={<MarvelPage />} />
         <Route path="dc" element={<DcPage />} />
---
+++ 06-HeroesSPA/src/ui/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import { Link, NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+      <Link className="navbar-brand" to="/">
+        Asociaciones
+      </Link>
+
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "actove" : ""}`
+            }
+            to="/marvel"
+          >
+            Marvel
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `nav-item nav-link ${isActive ? "actove" : ""}`
+            }
+            to="/dc"
+          >
+            DC
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+        <ul className="navbar-nav ml-auto">
+          <span className="nav-item nav-link text-primary">Arturo</span>
+          <button className="nav-item nav-link btn-primary">Logout</button>
+        </ul>
+      </div>
+    </nav>
+  );
+};
