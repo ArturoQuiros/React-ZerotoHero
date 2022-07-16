24a7a6384b30bfba1587c3fcefcac83940fe7068
Arturo Quirós <joseq2408@gmail.com>
2022-07-15 21:04:27 -0600

    redux added to journal app

--- 07-JournalApp/src/main.jsx
+++ 07-JournalApp/src/main.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import { JournalApp } from "./JournalApp";
 import { BrowserRouter } from "react-router-dom";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
