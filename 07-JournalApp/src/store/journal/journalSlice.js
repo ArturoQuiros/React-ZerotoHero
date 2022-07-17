a569ac984974fb93dac36a047d216b34b05991a0
Arturo Quirós <joseq2408@gmail.com>
2022-07-17 10:23:25 -0600

    journal reducer

---
+++ 07-JournalApp/src/store/journal/journalSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const journalSlice = createSlice({
+  name: "journal",
+  initialState: {
+    isSaving: true,
+    messageSaved: "",
+    notes: [],
+    active: null,
+    /*active: {
+      id: "",
+      title: "",
+      body: "",
+      date: 0,
+      imageURLs: [],
+    },*/
+  },
+  reducers: {
+    addNewEmptyNote: (state, action) => {},
+    setActiveNote: (state, action) => {},
+    setNotes: (state, action) => {},
+    setSaving: (state) => {},
+    updateNote: (state) => {},
+    deleteNoteById: (state) => {},
+  },
+});
+
+export const {
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteById,
+} = journalSlice.actions;
--- 07-JournalApp/src/store/store.js
+++ 07-JournalApp/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authSlice } from "./auth";
+import { journalSlice } from "./journal/journalSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    journal: journalSlice.reducer,
   },
 });
