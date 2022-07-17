cd4d9aaa2409d1e2dda3a2d4a6e3a5af835ae476
Arturo Quirós <joseq2408@gmail.com>
2022-07-16 19:08:14 -0600

    routes with auth

--- 07-JournalApp/src/router/AppRouter.jsx
+++ 07-JournalApp/src/router/AppRouter.jsx
@@ -1,12 +1,42 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { FirebaseAuth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
+
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/components/CheckingAuth";
+import { login, logout } from "../store/auth";
 
 export const AppRouter = () => {
+  const { status } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) {
+        return dispatch(logout());
+      }
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, []);
+
+  if (status === "checking") {
+    return <CheckingAuth />;
+  }
   return (
     <Routes>
-      <Route path="/auth/*" element={<AuthRoutes />} />
-      <Route path="/*" element={<JournalRoutes />} />
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+
+      {/* <Route path="/auth/*" element={<AuthRoutes />} />
+      <Route path="/*" element={<JournalRoutes />} />*/}
     </Routes>
   );
 };
--- 07-JournalApp/src/store/auth/authSlice.js
+++ 07-JournalApp/src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
-    status: "not-authenticated",
+    status: "checking",
     uid: null,
     email: null,
     displayName: null,
