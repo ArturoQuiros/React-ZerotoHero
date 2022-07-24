a3c8f9ef5f707581dc9e78e6316a4df2f3db1d3c
Arturo Quirós <joseq2408@gmail.com>
2022-07-24 12:36:37 -0600

    .env added

--- 10-CalendarBackend/index.js
+++ 10-CalendarBackend/index.js
@@ -1,16 +1,22 @@
 const express = require("express");
+require("dotenv").config();
 //express server
 const app = express();
 
 //rutas
+/*
 app.get("/", (req, res) => {
   console.log("hola mundo");
   res.json({
     ok: true,
   });
 });
+*/
+
+//public
+app.use(express.static("public"));
 
 //listen
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log("This is my server running");
 });
