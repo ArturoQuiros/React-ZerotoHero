b3b7e117d34c5b07758eee888b18542825b795f3
Arturo Quirós <joseq2408@gmail.com>
2022-07-24 12:24:52 -0600

    express added

--- 10-CalendarBackend/index.js
+++ 10-CalendarBackend/index.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo2");
+const express = require("express");
+//express server
+const app = express();
+
+//rutas
+app.get("/", (req, res) => {
+  console.log("hola mundo");
+  res.json({
+    ok: true,
+  });
+});
+
+//listen
+app.listen(4000, () => {
+  console.log("This is my server running");
+});
