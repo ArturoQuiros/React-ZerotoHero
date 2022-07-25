816295a1f09f1f7518ed41f8837f5d3ef2a93046
Arturo Quirós <joseq2408@gmail.com>
2022-07-25 10:59:16 -0600

    mongo connection added

---
+++ 10-CalendarBackend/database/config.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.DB_CNN, {});
+    console.log("DB Success");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  dbConnection,
+};
--- 10-CalendarBackend/index.js
+++ 10-CalendarBackend/index.js
@@ -1,7 +1,13 @@
+//requires
 const express = require("express");
 require("dotenv").config();
+const { dbConnection } = require("./database/config");
+
 //express server
 const app = express();
+
+//DB Connection
+dbConnection();
 
 //public
 app.use(express.static("public"));
@@ -12,5 +18,5 @@ app.use("/api/auth", require("./routes/auth"));
 
 //listen
 app.listen(process.env.PORT, () => {
-  console.log("This is my server running");
+  console.log("My server is running");
 });
