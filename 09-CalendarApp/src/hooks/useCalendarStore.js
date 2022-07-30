a11057a736ca88e82b75865b5f5d732c8e538613
Arturo Quirós <joseq2408@gmail.com>
2022-07-30 12:02:37 -0600

    create event on backend

--- 09-CalendarApp/src/hooks/useCalendarStore.js
+++ 09-CalendarApp/src/hooks/useCalendarStore.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import calendarApi from "../api/calendarApi";
 import {
   onAddNewEvent,
   onDeleteEvent,
@@ -8,6 +9,8 @@ import {
 
 export const useCalendarStore = () => {
   const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
+
   const dispatch = useDispatch();
 
   const setActiveEvent = (calendarEvent) => {
@@ -15,12 +18,16 @@ export const useCalendarStore = () => {
   };
 
   const startSavingEvent = async (calendarEvent) => {
+    //go to backend
+
     if (calendarEvent._id) {
       //update
       dispatch(onUpdateEvent({ ...calendarEvent }));
     } else {
       //create
-      dispatch(onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+      const { data } = await calendarApi.post("/events", calendarEvent);
+      console.log(data);
+      dispatch(onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
     }
   };
 
