51f6364d4a57755f70713b44124554bb073035b4
Arturo Quirós <joseq2408@gmail.com>
2022-07-30 14:00:55 -0600

    env for production fix

--- 09-CalendarApp/src/helpers/getENV.js
+++ 09-CalendarApp/src/helpers/getENV.js
@@ -1,8 +1,7 @@
 export const getENV = () => {
-  console.log(import.meta.env);
-  import.meta.env;
+  //import.meta.env;
 
   return {
-    ...import.meta.env,
+    VITE_API_URL: import.meta.env.VITE_API_URL,
   };
 };
