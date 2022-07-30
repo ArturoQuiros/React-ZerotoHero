b1a81b1b68b96a4c66c26d835e98d1693618478b
Arturo Quirós <joseq2408@gmail.com>
2022-07-30 13:29:20 -0600

    colors for users

--- 09-CalendarApp/src/calendar/pages/CalendarPage.jsx
+++ 09-CalendarApp/src/calendar/pages/CalendarPage.jsx
@@ -3,12 +3,16 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import { NavBar, CalendarModal, FabAddNew, FabDelete } from "../";
 import { localizer, getMessagesES } from "../../helpers";
 import { useEffect, useState } from "react";
-import { useUiStore, useCalendarStore } from "../../hooks";
+import { useUiStore, useCalendarStore, useAuthStore } from "../../hooks";
 
 export const CalendarPage = () => {
+  const { user } = useAuthStore();
   const eventStyleGetter = (event, start, end, isSelected) => {
+    const isMyEvent =
+      user.uid === event.user._id || user.uid === event.user.uid;
+
     const style = {
-      backgroundColor: "347CF7",
+      backgroundColor: isMyEvent ? "347CF7" : "455660",
       borderRadius: "0px",
       opacity: "0.8",
       color: "white",
